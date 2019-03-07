b68554f913447f69f6351c04eef638ed8852d9e5
Nick <nick@nhowlett.com>
2019-03-07 20:03:56 +0000

    Finish styling of watchlist add/remove button

--- frontend/components/stocks/watch_button.jsx
+++ frontend/components/stocks/watch_button.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 export default ({watchId, currentUser, watchStock, removeWatch, match}) => {
   if(!watchId){
     return(
-      <button onClick={() => watchStock(currentUser, match.params.symbol)}>Add to Watchlist</button>
+      <button id="watch-button" onClick={() => watchStock(currentUser, match.params.symbol)}>Add to Watchlist</button>
       )
   } else{
     return(
-      <button onClick={() => removeWatch(watchId)}>Remove from Watchlist</button>
+      <button id="watch-button" onClick={() => removeWatch(watchId)}>Remove from Watchlist</button>
     )
   }
 }
