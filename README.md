4da732a41f23aeaae5c82a781dc70d817c6747d7
Nick <nick@nhowlett.com>
2019-03-08 18:33:51 +0000

    Begin readme work

---
+++ README.md
@@ -0,0 +1,1 @@
+rc="https://raw.githubusercontent.com/Nick-Howlett/Sherwood/master/app/assets/images/sherwood_logo_green.png" width="50" height="50"> Sherwood
--- frontend/components/watchlist/watchlist_item.jsx
+++ frontend/components/watchlist/watchlist_item.jsx
@@ -9,7 +9,7 @@ export default props => {
           <h4>{props.stock.symbol}</h4>
           <div>{props.stock.shares ? `${props.stock.shares} Shares` : ""}</div>
         </div>
-        <LineChart width={60} height={16} margin={{top: 0, right: 0, bottom: 0, left: 0}} data={props.stock.chart}>
+        <LineChart width={60} height={20} margin={{top: 0, right: 0, bottom: 0, left: 0}} data={props.stock.chart}>
           <Line type={"linear"}
                 dataKey={"marketOpen"}
                 stroke="#21ce99" 
