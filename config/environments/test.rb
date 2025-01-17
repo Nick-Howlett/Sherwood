5cf8ca77897082be81f52862d3bc5c8f44078b9c
Nick Howlett <nick@nhowlett.com>
2025-01-16 23:00:14 -0800

    Commit for heroku deploy

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -11,6 +11,7 @@ Rails.application.configure do
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
   config.eager_load = false
+  
 
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
