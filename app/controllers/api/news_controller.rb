61b1b89ff641a2cddd6fc5711cbacf657fde7f73
Nick Howlett <nick@nhowlett.com>
2025-01-26 13:00:12 -0800

    Correctly string format api requests to news api

--- app/controllers/api/news_controller.rb
+++ app/controllers/api/news_controller.rb
@@ -5,14 +5,14 @@ class Api::NewsController < ApplicationController
     def show
         name = params[:name]
         api_key = Rails.application.credentials.news
-        uri = URI.parse('https://newsapi.org/v2/everything?q=#{name}&language=en&apiKey=#{api_key}&pageSize=5')
+        uri = URI.parse("https://newsapi.org/v2/everything?q=#{name}&language=en&apiKey=#{api_key}&pageSize=5")
         Net::HTTP.get(uri)
     end
 
 
     def index
         api_key = Rails.application.credentials.news
-        uri = URI.parse('https://newsapi.org/v2/top-headlines?category=business&country=us&apiKey=#{api_key}&pageSize=5')
+        uri = URI.parse("https://newsapi.org/v2/top-headlines?category=business&country=us&apiKey=#{api_key}&pageSize=5")
         Net::HTTP.get(uri)
     end
 end
