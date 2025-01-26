00229d56c9384a38ee935238eb0dc989b7d82ef0
Nick Howlett <nick@nhowlett.com>
2025-01-26 14:05:40 -0800

    correct local variable naming

--- app/controllers/api/stock_api_controller.rb
+++ app/controllers/api/stock_api_controller.rb
@@ -1,7 +1,7 @@
 class Api::StockApiController < ApplicationController
     def show
         method = params[:method]
-        symbols = params[:symbol]
+        symbol = params[:symbol]
         from_date = params[:from_date]
         to_date = params[:to_date]
         api_key = Rails.application.credentials.stocks
@@ -9,9 +9,9 @@ class Api::StockApiController < ApplicationController
         when 'intraday'
             url = "https://api.marketstack.com/v2/intraday/latest?symbols=#{symbol}&interval=5min&access_key=#{api_key}"
         when 'historical'
-            url = "https://api.marketstack.com/v2/eod?access_key=#{api_key}&symbols=#{symbols}&date_from=#{from_date}&date_to=#{to_date}"
+            url = "https://api.marketstack.com/v2/eod?access_key=#{api_key}&symbols=#{symbol}&date_from=#{from_date}&date_to=#{to_date}"
         when 'info'
-            url = "https://api.marketstack.com/v2/eod?access_key=#{api_key}&symbols=#{symbols}&date_from=#{from_date}"
+            url = "https://api.marketstack.com/v2/eod?access_key=#{api_key}&symbols=#{symbol}&date_from=#{from_date}"
         end
         uri = URI.parse(url)
         @response = Net::HTTP.get(uri)
