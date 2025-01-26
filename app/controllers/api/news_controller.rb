f254e19169724b877629b78aa1ce2822db985db8
Nick Howlett <nick@nhowlett.com>
2025-01-26 12:48:19 -0800

    Use correct get argument for news api

--- app/controllers/api/news_controller.rb
+++ app/controllers/api/news_controller.rb
@@ -4,13 +4,13 @@ class Api::NewsController < ApplicationController
     def show
         @name = params[:name]
         @api_key = Rails.application.credentials.news
-        Net::HTTP::Get('https://newsapi.org/v2/everything?q=#{name}&language=en&apiKey=#{api_key}&pageSize=5')
+        Net::HTTP.get('https://newsapi.org/v2/everything?q=#{name}&language=en&apiKey=#{api_key}&pageSize=5')
     end
 
 
     def index
         @api_key = Rails.application.credentials.news
-        Net::HTTP::Get('https://newsapi.org/v2/top-headlines?category=business&country=us&apiKey=#{api_key}&pageSize=5')
+        Net::HTTP.get('https://newsapi.org/v2/top-headlines?category=business&country=us&apiKey=#{api_key}&pageSize=5')
     end
 end
   
