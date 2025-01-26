966d2ab74fa77fce0094ff35c32a14ba85641d44
Nick Howlett <nick@nhowlett.com>
2025-01-26 15:10:34 -0800

    Update stock show page to match new API

--- frontend/components/stocks/stock_show.jsx
+++ frontend/components/stocks/stock_show.jsx
@@ -1,65 +1,66 @@
-import React from 'react';
-import ChartContainer from '../chart/chart_container';
-import StockAbout from './stock_about';
-import Navbar from '../navbar/navbar';
+import React from "react";
+import ChartContainer from "../chart/chart_container";
+import StockAbout from "./stock_about";
+import Navbar from "../navbar/navbar";
 import News from "./news";
-import StockTransactionContainer from './stock_transaction_container';
-import Loading from '../loading';
+import StockTransactionContainer from "./stock_transaction_container";
+import Loading from "../loading";
 import WatchButtonContainer from "./watch_button_container";
-import Footer from '../footer';
+import Footer from "../footer";
 
 class StockShow extends React.Component {
-  
-  componentDidMount(){
+  componentDidMount() {
     this.getData(this.props.match.params.symbol);
   }
 
-  getData(symbol){
+  getData(symbol) {
     this.props.getSearch().then(() => {
-      this.props.getStockDisplay(symbol).then(this.props.getStockHistoricalCharts(symbol));
+      this.props
+        .getStockDisplay(symbol)
+        .then(this.props.getStockHistoricalCharts(symbol));
       this.props.getNews(this.props.stocks[symbol.toUpperCase()].name);
     });
   }
 
-  componentDidUpdate(prevProps){
-    if(prevProps.match.params.symbol !== this.props.match.params.symbol){
+  componentDidUpdate(prevProps) {
+    if (prevProps.match.params.symbol !== this.props.match.params.symbol) {
       this.getData(this.props.match.params.symbol);
     }
   }
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     this.props.clearCharts();
   }
 
-
-  render(){
-    if(!this.props.info ||
-       !this.props.info.price ||
-       !this.props.news){
-      return <Loading id={"loading-center"}/>
-    } 
+  render() {
+    if (!this.props.info || !this.props.info.close || !this.props.news) {
+      return <Loading id={"loading-center"} />;
+    }
     return (
       <>
         <div className="fixed navbar">
-          <Navbar/>
+          <Navbar />
         </div>
         <main id="main-page">
           <div>
-            <ChartContainer/>
+            <ChartContainer />
             <StockAbout info={this.props.info} />
             <News news={this.props.news} />
           </div>
           <div id="side-column">
             <div className="fixed">
-              <StockTransactionContainer symbol={this.props.info.symbol} price={this.props.info.price}/>
-              {this.props.currentUser ? <WatchButtonContainer/> : ""}
+              <StockTransactionContainer
+                symbol={this.props.info.symbol}
+                price={this.props.info.close}
+              />
+              {this.props.currentUser ? <WatchButtonContainer /> : ""}
             </div>
           </div>
         </main>
-        <Footer/>
+        <Footer />
       </>
-    )
+    );
   }
-};
+}
 
 export default StockShow;
