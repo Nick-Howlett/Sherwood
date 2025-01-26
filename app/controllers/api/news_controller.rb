a60032f743e4c58f66c54af798bb01f31e1d406c
Nick Howlett <nick@nhowlett.com>
2025-01-26 12:51:56 -0800

    parse string into uri for api

--- app/controllers/api/news_controller.rb
+++ app/controllers/api/news_controller.rb
@@ -1,16 +1,19 @@
 require 'net/http'
+require 'uri'
 
 class Api::NewsController < ApplicationController
     def show
-        @name = params[:name]
-        @api_key = Rails.application.credentials.news
-        Net::HTTP.get('https://newsapi.org/v2/everything?q=#{name}&language=en&apiKey=#{api_key}&pageSize=5')
+        name = params[:name]
+        api_key = Rails.application.credentials.news
+        uri = URI.parse('https://newsapi.org/v2/everything?q=#{name}&language=en&apiKey=#{api_key}&pageSize=5')
+        Net::HTTP.get(uri)
     end
 
 
     def index
-        @api_key = Rails.application.credentials.news
-        Net::HTTP.get('https://newsapi.org/v2/top-headlines?category=business&country=us&apiKey=#{api_key}&pageSize=5')
+        api_key = Rails.application.credentials.news
+        uri = URI.parse('https://newsapi.org/v2/top-headlines?category=business&country=us&apiKey=#{api_key}&pageSize=5')
+        Net::HTTP.get(uri)
     end
 end
   
