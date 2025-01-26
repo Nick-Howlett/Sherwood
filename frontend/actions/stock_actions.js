6850460eaab9d9c87800a5d9530c3d6693abee74
Nick Howlett <nick@nhowlett.com>
2025-01-26 14:25:52 -0800

    Fix chart data

--- frontend/actions/stock_actions.js
+++ frontend/actions/stock_actions.js
@@ -36,7 +36,7 @@ export const getStockDisplay =
           APIUtil.getInfo(symbol),
           APIUtil.getIntradayChart(symbol),
         ]).then((valuesResp) => {
-          const values = valuesResp.map((resp) => resp.response);
+          const values = valuesResp.map((resp) => resp?.response);
           const info = values.slice(0, 2);
           const chart = formatChart(values[2]);
           info.forEach((info) => {
