286bc0cadcccb581cba8bd3bfae464cf6e708d5d
Nick Howlett <nick@nhowlett.com>
2025-01-26 13:52:53 -0800

    Add stock api and controller

---
+++ app/controllers/api/stock_apis_controller.rb
@@ -0,0 +1,23 @@
+class Api::StockApisController < ApplicationController
+    def show
+        method = params[:method]
+        symbols = params[:symbol]
+        from_date = params[:from_date]
+        to_date = params[:to_date]
+        api_key = Rails.application.credentials.stocks
+        case method
+        when 'intraday':
+            url = "https://api.marketstack.com/v2/intraday/latest?symbols=#{symbol}&interval=5min&access_key=#{api_key}"
+        when 'historical':
+            url = "https://api.marketstack.com/v2/eod?access_key=#{api_key}&symbols=#{symbols}&date_from=#{from_date}&date_to=#{to_date}"
+        when 'info':
+            url = "https://api.marketstack.com/v2/eod?access_key=#{api_key}&symbols=#{symbols}&date_from=#{from_date}"
+            
+        uri = URI.parse(url)
+        @response = Net::HTTP.get(uri)
+        render 'api/stock_api/show'
+    end
+
+  
+  end
+  
